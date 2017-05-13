a3e4ffeb733192143ec532bda8dd1ed4c5007d51
gitzx <emailzhx@126.com>
2017-05-13 15:09:30 +0800

    add Find_All_Duplicates_In_An_Array

--- LeetCode_Python/Array/Find_All_Duplicates_In_An_Array.py
+++ LeetCode_Python/Array/Find_All_Duplicates_In_An_Array.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*
+
 '''
 Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
 
@@ -13,3 +15,33 @@ Example:
     [2,3]
 
 '''
+
+class Solution(object):
+	def findAllDuplicatesInAnArray(self, nums):
+		result = []
+		for n in nums:
+			if nums[abs(n) - 1] < 0:
+				result.append(abs(n))
+			else:
+				nums[abs(n) - 1] *= -1
+		return result
+
+	def findAllDuplicatesInAnArray2(self, nums):
+		result = []
+		for i in range(len(nums)):
+			while nums[i] and nums[i] != i+1:
+				n = nums[i]
+				if nums[i] == nums[n-1]:
+					result.append(n)
+					nums[i] = 0
+				else:
+					nums[i], nums[n-1] = nums[n-1], nums[i]
+		return result
+
+if __name__ == '__main__':
+	nums = [4,3,2,7,8,2,3,1]
+	result = Solution().findAllDuplicatesInAnArray(nums)
+	print result  #[2, 3]
+
+
+
