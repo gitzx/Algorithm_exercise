eb00b88ff2820f4ddf70a59c023bd6716afc087d
gitzx <emailzhx@126.com>
2017-05-15 18:40:48 +0800

    add leetcode_python

--- LeetCode_Python/Array/Max_Consecutive_Ones.py
+++ LeetCode_Python/Array/Max_Consecutive_Ones.py
@@ -15,4 +15,33 @@ The length of input array is a positive integer and will not exceed 10,000
 
 class Solution(object):
 	def maxConsecutiveOnes(self, nums):
-		
+		result = []
+		lens = []
+		for n in nums:
+			if n == 1:
+				result.append(n)
+			else:
+				lens.append(len(result))
+				result = []
+		lens.append(len(result))
+		result = []
+		maxLen = 0
+		for l in lens:
+			if l > maxLen:
+				maxLen = l
+		return maxLen
+
+	def maxConsecutiveOnes2(self, nums):
+		ans = cnt = 0
+		for n in nums:
+			if n == 1:
+				cnt += 1
+				ans = max(ans, cnt)
+			else:
+				cnt = 0
+		return ans
+
+if __name__ == '__main__':
+	nums = [1,1,0,1,1,1]
+	result = Solution().maxConsecutiveOnes(nums)
+	print result  # 3
