2cb8979d4901103eb23125d5e40913ca96744c46
gitzx <emailzhx@126.com>
2017-05-23 10:23:49 +0800

    add Triangle

--- LeetCode_Python/Array/Triangle.py
+++ LeetCode_Python/Array/Triangle.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
 
@@ -15,3 +17,29 @@ Bonus point if you are able to do this using only O(n) extra space, where n is t
 
 '''
 
+class Solution(object):
+	# dp
+	def minimumTotal(self, triangle):
+		"""
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+		if len(triangle) == 0 : 
+			return 0
+
+		res = [0 for i in range(len(triangle))]
+		res[0] = triangle[0][0]
+		for i in range(1, len(triangle)):
+			for j in range(len(triangle[i])-1, -1, -1):
+				if j == len(triangle[i]) -1:
+					res[j] = res[j-1] + triangle[i][j]
+				elif j == 0:
+					res[j] = res[j] + triangle[i][j]
+				else:
+					res[j] = min(res[j-1], res[j]) + triangle[i][j]
+		return min(res)
+
+if __name__ == '__main__':
+	triangle = [[2], [3,4], [6,5,7], [4,1,8,3]]
+	result = Solution().minimumTotal(triangle)
+	print result  # 11
