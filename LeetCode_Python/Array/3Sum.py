5de607f1491ae9cf63ab7205e2607e002aee7888
gitzx <emailzhx@126.com>
2017-05-26 17:44:35 +0800

    add 3Sum

--- LeetCode_Python/Array/3Sum.py
+++ LeetCode_Python/Array/3Sum.py
@@ -16,4 +16,32 @@ A solution set is:
 
 class Solution(object):
 	def threeSum(self, nums):
-		
+		nums.sort()
+		result = []
+		length = len(nums)
+		if length < 3:
+			return result
+		for i in range(0, length -2):
+			if i > 0  and nums[i] == nums[i-1]:
+				continue
+			target = -1 * nums[i]
+			left, right = i+1, length - 1
+			while left < right:
+				if nums[left] + nums[right] == target:
+					result.append([nums[i], nums[left], nums[right]])
+					right -= 1
+					left += 1
+					while left < right and nums[left] == nums[left-1]:
+						left += 1
+					while left < right and nums[right] == nums[right + 1]:
+						right -= 1
+				elif nums[left] + nums[right] > target:
+					right -= 1
+				else:
+					left += 1
+		return result
+
+if __name__ == '__main__':
+	nums = [-1, 0, 1, 2, -1, -4]
+	result = Solution().threeSum(nums)
+	print result  # [[-1, -1, 2], [-1, 0, 1]]
