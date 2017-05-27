52e0de24e4287d7e6e8140d4ce44d61d5b40aee5
gitzx <emailzhx@126.com>
2017-05-27 17:19:05 +0800

    add 3Sum_Closest

--- LeetCode_Python/Array/3Sum_Closest.py
+++ LeetCode_Python/Array/3Sum_Closest.py
@@ -7,3 +7,30 @@ Return the sum of the three integers. You may assume that each input would have 
     The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
 '''
+
+class Solution(object):
+	def threeSumClosest(self, nums, target):
+		nums.sort()
+		mindiff = 100000
+		result = None
+		for i in range(len(nums)):
+			left, right = i + 1, len(nums) - 1
+			while left < right:
+				total = nums[left] + nums[right] + nums[i]
+				diff = abs(total - target)
+				if result is None or diff < mindiff:
+					mindiff = diff
+					result = total
+				if total == target:
+					return total
+				elif total < target:
+					left += 1
+				else:
+					right -= 1
+		return result
+
+if __name__ == '__main__':
+	nums = [-1, 2, 1, -4]
+	result = Solution().threeSumClosest(nums, 1)
+	print result
+
