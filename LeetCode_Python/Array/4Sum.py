b9f9b5fb419cfea01a02ce7df1b8869c99b69853
gitzx <emailzhx@126.com>
2017-05-31 12:58:35 +0800

    add 4Sum

--- LeetCode_Python/Array/4Sum.py
+++ LeetCode_Python/Array/4Sum.py
@@ -17,4 +17,26 @@ A solution set is:
 
 class Solution(object):
 	def fourSum(self, nums, target):
-		
+		result = []
+		nums.sort()
+		for i in range(0, len(nums) - 3):
+			for j in range(i+1, len(nums) - 2):
+				p = j + 1
+				q = len(nums) -1
+				while p != q:
+					summer = nums[i] + nums[j] + nums[p] + nums[q]
+					if summer == target:
+						tempList = [nums[i], nums[j], nums[p], nums[q]]
+						if tempList not in result:
+							result.append(tempList)
+						p += 1
+					elif summer > target:
+						q -= 1
+					else:
+						p += 1
+		return result
+
+if __name__ == '__main__':
+	nums = [1, 0, -1, 0, -2, 2]
+	result = Solution().fourSum(nums, 0)
+	print result  # [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
