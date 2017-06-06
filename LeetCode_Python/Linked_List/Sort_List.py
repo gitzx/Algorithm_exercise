d29a1d268de0256e2448552990c1835ac6b0dc3e
gitzx <emailzhx@126.com>
2017-06-06 20:39:50 +0800

    add Sort_List

--- LeetCode_Python/Linked_List/Sort_List.py
+++ LeetCode_Python/Linked_List/Sort_List.py
@@ -12,3 +12,38 @@ class ListNode(object):
 
 class Solution(object):
 	def sortList(self, head):
+		if head is None or head.next is None:
+			return head
+		mid = self.getMiddle(head)
+		rHead = mid.next
+		mid.next = None
+		return self.merge(self.sortList(head), self.sortList(rHead))
+
+	def getMiddle(self, head):
+		if head is None:
+			return head
+		slow = head
+		fast = head
+		while fast.next and fast.next.next:
+			slow = slow.next
+			fast = fast.next.next
+		return slow
+
+	def merge(self, lHead, rHead):
+		dummyNode = ListNode(0)
+		dummyHead = dummyNode
+		while lHead and rHead:
+			if lHead.val < rHead.val:
+				dummyHead.next = lHead
+				lHead = lHead.next
+			else:
+				dummyHead.next = rHead
+				rHead = rHead.next
+			dummyHead = dummyHead.next
+		if lHead:
+			dummyHead.next = lHead
+		else:
+			dummyHead.next = rHead
+		return dummyNode.next
+
+
