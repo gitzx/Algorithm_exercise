78059a727a699921e32087c64ade5d29451c794b
gitzx <emailzhx@126.com>
2017-06-08 19:18:05 +0800

    add Reorder_List

--- LeetCode_Python/Linked_List/Reorder_List.py
+++ LeetCode_Python/Linked_List/Reorder_List.py
@@ -1,3 +1,22 @@
 '''
+Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You must do this in-place without altering the nodes' values.
+
+For example,
+Given {1,2,3,4}, reorder it to {1,4,2,3}.
 
 '''
+
+'''
+class ListNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+'''
+
+class Solution(object):
+	def reorderList(self, head):
+
+		
