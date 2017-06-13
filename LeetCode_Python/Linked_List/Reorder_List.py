0b263a9d73a6c5b36efb10f375e918a0bebca8dc
gitzx <emailzhx@126.com>
2017-06-13 14:14:14 +0800

    add Reorder_List

--- LeetCode_Python/Linked_List/Reorder_List.py
+++ LeetCode_Python/Linked_List/Reorder_List.py
@@ -18,6 +18,33 @@ class ListNode(object):
 
 class Solution(object):
 	def reorderList(self, head):
-		if head==None or head.next==None or head.next.next==None: 
-			return head
+		if head is None or head.next is None:
+			return
 		
+		slow = fast = head
+		while fast and fast.next:
+			slow = slow.next
+			fast = fast.next.next
+		head1 = head
+		head2 = slow.next
+		slow.next = None
+
+		dummy = ListNode(0)
+		p = head2
+		while p:
+			next = p.next
+			p.next = dummy.next
+			dummy.next = p
+			p = next
+		head2 = dummy.next
+
+		p1 = head1
+		p2 = head2
+		while p2:
+			temp1 = p1.next
+			temp2 = p2.next
+			p1.next = p2
+			p2.next = temp1
+			p1 = temp1
+			p2 = temp2
+
