6f433b8cf7d029362387f741cc0cba5416bc19c1
gitzx <emailzhx@126.com>
2017-06-19 19:11:52 +0800

    add Linked_List_Cycle_II

--- LeetCode_Python/Linked_List/Linked_List_Cycle_II.py
+++ LeetCode_Python/Linked_List/Linked_List_Cycle_II.py
@@ -1,3 +1,19 @@
 '''
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+Follow up:
+Can you solve it without using extra space?
 
 '''
+
+'''
+class ListNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+'''
+
+class Solution(object):
+	def detectCycle(self, head):
