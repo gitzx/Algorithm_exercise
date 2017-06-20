39cbe7385ca018c2332ee3bdbc0b96e93b99391a
gitzx <emailzhx@126.com>
2017-06-20 19:32:12 +0800

    add Copy_List_With_Random_Pointer

--- LeetCode_Python/Linked_List/Copy_List_With_Random_Pointer.py
+++ LeetCode_Python/Linked_List/Copy_List_With_Random_Pointer.py
@@ -1,3 +1,20 @@
 '''
+A linked list is given such that each node contains an additional random pointer 
+which could point to any node in the list or null.
+
+Return a deep copy of the list.
 
 '''
+
+'''
+class RandomListNode(object):
+	def __init__(self, x):
+	    self.label = x
+        self.next = None
+        self.random = None
+        
+'''
+
+class Solution(object):
+	def copyRandomList(self, head):
+		
