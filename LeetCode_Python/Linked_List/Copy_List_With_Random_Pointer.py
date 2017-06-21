7c18a4de06b79052c8efc7333466d85775bf4da2
gitzx <emailzhx@126.com>
2017-06-21 17:42:55 +0800

    add Copy_List_With_Random_Pointer

--- LeetCode_Python/Linked_List/Copy_List_With_Random_Pointer.py
+++ LeetCode_Python/Linked_List/Copy_List_With_Random_Pointer.py
@@ -17,4 +17,47 @@ class RandomListNode(object):
 
 class Solution(object):
 	def copyRandomList(self, head):
-		
+		if head == None:
+			return None
+		tmp = head
+		while tmp:
+			newNode = RandomListNode(tmp.label)
+			newNode.next = tmp.next
+			tmp.next = newNode
+			tmp = tmp.next.next
+		tmp = head
+		while tmp:
+			if tmp.random:
+				tmp.next.random = tmp.random.next
+			tmp = tmp.next.next
+		newhead = head.next
+		pold = head
+		pnew = newhead
+		while pnew.next:
+			pold.next = pnew.next
+			pold = pold.next
+			pnew.next = pold.next
+			pnew = pnew.next
+		pold.next = None
+		pnew.next = None
+		return newhead
+
+	def copyRandomList2(self, head):
+		nodeDict = dict()
+		dummy = RandomListNode(0)
+		pointer, newhead = head, dummy
+		while pointer:
+			newNode = RandomListNode(pointer.label)
+			newhead.next = newNode
+			nodeDict[pointer] = newhead.next
+			newhead = newhead.next
+			pointer = pointer.next
+		pointer, newhead = head, dummy.next
+		while pointer:
+			if pointer.random:
+				newhead.random = nodeDict[pointer.random]
+			pointer, newhead = pointer.next, newhead.next
+		return dummy.next
+
+
+
