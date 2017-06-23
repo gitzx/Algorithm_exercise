8539d654a500638d97d1cd73ce08f7005e041401
gitzx <emailzhx@126.com>
2017-06-23 17:52:50 +0800

    add Merge_K_Sorted_Lists

--- LeetCode_Python/Linked_List/Merge_K_Sorted_Lists.py
+++ LeetCode_Python/Linked_List/Merge_K_Sorted_Lists.py
@@ -1,3 +1,15 @@
 '''
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
 
 '''
+
+'''
+class ListNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+'''
+
+class Solution(object):
+	def mergeKLists(self, lists):
+		
