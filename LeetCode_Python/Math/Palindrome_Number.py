6b77b2716b220a457b23d8c8b670f542c8e93d28
gitzx <emailzhx@126.com>
2017-07-08 11:33:16 +0800

    add Leetcode_Python_Math

--- LeetCode_Python/Math/Palindrome_Number.py
+++ LeetCode_Python/Math/Palindrome_Number.py
@@ -14,4 +14,17 @@ There is a more generic way of solving this problem.
 
 class Solution(object):
 	def isPalindrome(self, x):
+		if x < 0:
+			return False
+		div = 1
+		while x/div >= 10:
+			div = div * 10
+		while x:
+			left = x/div
+			right = x % 10
+			if left != right:
+				return False
+			x = (x % div)/10
+			div = div/100
+		return True
 		
