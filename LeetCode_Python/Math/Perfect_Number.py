f8944a89b39f2d227552d47d8cda2a17919b7da3
gitzx <emailzhx@126.com>
2017-07-10 19:35:55 +0800

    add LeetCode_Python_Math

--- LeetCode_Python/Math/Perfect_Number.py
+++ LeetCode_Python/Math/Perfect_Number.py
@@ -15,5 +15,13 @@ Note: The input number n will not exceed 100,000,000. (1e8)
 
 class Solution(object):
 	def checkPerfectNumber(self, num):
-		
+		total, div = 1, 2
+		while div * div <= num:
+			if num % div == 0:
+				total += div
+				if div * div != num:
+					total += num/div
+			div += 1
+		return num > 1 and total == num
+
 
